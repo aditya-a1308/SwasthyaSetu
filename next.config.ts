3b836fce3619a996b19322517ca11368aa148bf6
Aditya Aware <adityaaware51@gmail.com>
2026-05-23 23:56:04 +0530

    language push

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  allowedDevOrigins: ["13.207.61.7"],
+};
 
 export default nextConfig;
